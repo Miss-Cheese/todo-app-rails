dd8b9ab961f40b36ac6c13ed735afa6d8a66e8ca
Miss-Cheese <nat.galligan@gmail.com>
2023-06-15 18:20:21 -0400

    Add Tasks controller and related

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,19 @@
+class TasksController < ApplicationController
+  def index
+    # show only tasks of the logged in user
+    render json: Task.all
+  end
+
+  def create
+  end
+
+  def show
+    byebug
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
